11c05d3cb1f00bcc26029b6bfc502c21ab1ddcea
aaronps <aaronperezsanchez@hotmail.com>
2018-05-02 12:38:07 +0800

    halfdone tuplelist, switching to computer

---
+++ python/tuplelist.py
@@ -0,0 +1,282 @@
+
+class TupleList:
+    """A List implementation using a tuple as container."""
+
+    def __init__(self, *items):
+        """Creates a list with optional initial elements"""
+        self.root = items
+
+    def __str__(self):
+        """Renders the list as String"""
+        return 'TupleList' + self.root.__str__()
+        
+    def __len__(self):
+        """Counts the number of items on the List""" 
+        return len(self.root)
+
+    def __getitem__(self, item):
+        """Basic __getitem__ for indexed access"""
+        if type(item) is slice:
+            raise TypeError
+
+        return self.root[item]
+
+    def prepend(self, value):
+        """Prepends an item at the beginning of the List"""
+        self.root = (value,) + self.root
+        return self
+
+    def append(self, value):
+        """Appends an item at the end of the List"""
+        self.root = self.root + (value,)
+        return self
+
+    def at(self, index):
+        """Returns the item at the specified index of the List"""
+        return self.root[index]
+
+    def delete(self, index, count=1):
+        """Deletes count item from index"""
+        # TODO
+        if index == 0:
+            while count > 0 and self.root is not None:
+                count = count - 1
+                self.root = self.root.next
+
+        else:
+            offset = 1
+            it = self.root
+
+            while offset < index and it is not None:
+                offset = offset + 1
+                it = it.next
+
+            if it is not None:
+                while count > 0 and it.next is not None:
+                    count = count - 1
+                    it.next = it.next.next
+
+        return self
+
+    def remove(self, value):
+        """Removes first item matching value"""
+        for index, v in enumerate(self.root):
+            if v == value:
+                self.delete(index)
+                break
+
+        return self
+
+    def remove_all(self, value):
+        """Removes all items matching value"""
+        while self.root is not None and self.root.value == value:
+            self.root = self.root.next
+
+        if self.root is not None:
+            it = self.root
+            while it.next is not None:
+                if it.next.value == value:
+                    it.next = it.next.next
+                else:
+                    it = it.next
+                            
+        return self
+
+    def set(self, index, value):
+        """Sets element at index to value (if it exists)"""
+        it = self.root
+        while it is not None and index > 0:
+            index = index - 1
+            it = it.next
+
+        if it is not None:
+            it.value = value
+
+        return self
+
+    def insert(self, index, value):
+        """Inserts value at specified index"""
+        if index == 0:
+            self.root = Node(value, self.root)
+        else:
+            it = self.root
+            count = 1
+            while count < index and it.next is not None:
+                count = count + 1
+                it = it.next
+
+            it.next = Node(value, it.next)
+
+        return self
+
+    def reverse(self):
+        """Returns a new list with the result of reversing this list"""
+        new_list = LinkedList()
+        
+        it = self.root
+        while it is not None:
+            new_list.prepend(it.value)
+            it = it.next
+            
+        return new_list
+
+    def clear(self):
+        """Clears the contents of the list"""
+        self.root = None
+        return self
+
+    def copy(self):
+        """Returns a copy of the list"""
+        result = LinkedList()
+        it = self.root
+        while it is not None:
+            # non efficient and lazy way :)
+            result.append(it.value)
+            it = it.next
+            
+        return result
+
+    def insert_sorted(self, value):
+        """Inserts value on the list keeping its order"""
+        if self.root is None or value <= self.root.value:
+            self.root = Node(value, self.root)
+            return self
+
+        it = self.root
+        while it.next is not None and value > it.next.value:
+            it = it.next
+    
+        it.next = Node(value, it.next)
+        return self
+
+    def sort_insert(self):
+        """Sorts the list"""
+        if self.root is None or self.root.next is None:
+            return self
+
+        new_list = LinkedList(self.root.value)
+        it = self.root.next
+        while it is not None:
+            new_list.insert_sorted(it.value)
+            it = it.next
+        
+        self.root = new_list.root
+        return self
+
+    def sort_bubble(self):
+        if self.root is None or self.root.next is None:
+            return self
+
+        swapped = True
+        while swapped:
+            swapped = False
+            it = self.root
+            while it.next is not None:
+                if it.value > it.next.value:
+                    it.next.value, it.value = it.value, it.next.value
+                    swapped = True
+                it = it.next
+                
+        return self
+
+    def sort_merge(self):
+        if self.root is None or self.root.next is None:
+            return self
+
+        # lol = list of lists
+        lol = LinkedList()
+        it = self.root
+
+        # step 1: sort elements by pair
+        # I'm reusing the Nodes
+        while it is not None and it.next is not None:
+            if it.value <= it.next.value:
+                # these two nodes are already in order, just push the first one
+                # and we will adjust links after that
+                lol.prepend(it)
+
+                # tricky line:
+                # 1- The second node next link to None
+                # 2- The iterator jumps two nodes
+                it.next.next, it = None, it.next.next
+
+            else:
+                # We have to swap both nodes and adjust the links
+                # first push the second node
+                lol.prepend(it.next)
+
+                # tricky line, MUST be in this order:
+                # 1- The second node next link becomes the first node
+                # 2- The first node next link becomes None
+                # 3- The iterator jumps two nodes
+                it.next.next, it.next, it = it, None, it.next.next
+
+        if it is not None:
+            lol.prepend(it)
+
+        # Step 2: merge sublists two by two
+        # also reusing the Nodes
+        while lol.root.next is not None:
+            it, lol.root = lol.root, None
+            while it is not None and it.next is not None:
+                lol.prepend(merge_nodes(it.value, it.next.value))
+                it = it.next.next
+
+            if it is not None:
+                lol.prepend(it)
+
+        self.root = lol.root.value
+        return self
+
+
+if __name__ == '__main__':
+    print('LinkedList example')
+
+    print("new empty", LinkedList())
+    print("new one element", LinkedList(1))
+    print("new multiple elements", LinkedList(1, 2, 3, 4, 5))
+
+    print("len empty", len(LinkedList()))
+    print("len one element", len(LinkedList(1)))
+    print("len multiple elements", len(LinkedList(1, 2, 3, 4, 5)))
+
+    li = LinkedList()
+    print('li =', li)
+    print('li.prepend(1) =', li.prepend(1))
+    print('li.prepend(2) =', li.prepend(2))
+    print('li.prepend(3) =', li.prepend(3))
+    print('li.append(2) =', li.append(2))
+    print('li.append(3) =', li.append(3))
+    print('li.at(2) =', li.at(2))
+    print('li.at(8) =', li.at(8))
+    print('li.copy().delete(1,3) =', li.copy().delete(1, 3))
+    print('li.delete(2) =', li.delete(2))
+    print('li.remove(2) =', li.remove(2))
+    print('li.remove_all(3) =', li.remove_all(3))
+
+    li2 = LinkedList('a', 'b', 'c', 'd')
+    print('li2 =', li2)
+    print('li2.set(0,"xxx") =', li2.set(0, "xxx"))
+    print('li2.set(8,"out") =', li2.set(8, "out"))
+    print('li2.set(3,"zzz") =', li2.set(3, "zzz"))
+    print('li2.set(2,"okk") =', li2.set(2, "okk"))
+    print('li2.insert(2,"middle") =', li2.insert(2, "middle"))
+    print('li2.insert(0,"start") =', li2.insert(0, "start"))
+    print('li2.insert(8,"end") =', li2.insert(8, "end"))
+
+    print('reverse 1,2,3 =', LinkedList(1, 2, 3).reverse())
+
+    print('li2[2] = ', li2[2])
+
+    insort = LinkedList()
+    print(insort.insert_sorted(2))
+    print(insort.insert_sorted(5))
+    print(insort.insert_sorted(4))
+    print(insort.insert_sorted(1))
+    print(insort.insert_sorted(6))
+    print(insort.insert_sorted(3))
+
+    print('insort =', insort)
+    print('sort_insert =', LinkedList(3, 6, 4, 7, 1, 2, 5, 0).sort_insert())
+    print('sort_bubble =', LinkedList(3, 6, 4, 7, 1, 2, 5, 0).sort_bubble())
+    print('sort_merge  =', LinkedList(6, 3, 4, 2, 1, 5, 0).sort_merge())
